3fe42ce2f26a476233956da33b818f112f62ac53
Huzaifa Naseer Khan <huzaifanaseerkhan02@gmail.com>
2024-06-14 15:28:37 +0500

    Added active class to active path

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,10 +1,13 @@
+"use client";
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
 
 const MainHeader = () => {
+  const path = usePathname();
   return (
     <>
       <MainHeaderBackground />
@@ -17,10 +20,22 @@ const MainHeader = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <Link
+                href="/meals"
+                className={
+                  path.startsWith("/meals") ? classes.active : undefined
+                }
+              >
+                Browse Meals
+              </Link>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <Link
+                href="/community"
+                className={path === "/community" ? classes.active : undefined}
+              >
+                Foodies Community
+              </Link>
             </li>
           </ul>
         </nav>
