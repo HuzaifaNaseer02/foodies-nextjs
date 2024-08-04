7506023a1a82609aca24175debabc049c7462520
Huzaifa Naseer Khan <huzaifanaseerkhan02@gmail.com>
2024-08-05 01:22:53 +0500

    Added server side form validation and metadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,12 @@ import MealsGrid from '@/components/meals/meals-grid'
 import { getMeals } from '@/lib/meals'
 import { Suspense } from 'react'
 
+export const metadata = {
+  title: 'All Meals',
+  description:
+    'Browse all available meals and choose your favourite one to cook today!'
+}
+
 async function Meals() {
   const meals = await getMeals()
   return <MealsGrid meals={meals} />
